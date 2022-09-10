b0b10a643ae199e78dd49c08962761a44611d489
MusaSkuru <captainskuru@gmail.com>
2022-09-10 06:03:57 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,7 @@
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
- * Return: 0
+ * Return: Always 0 (success/correct)
  */
 int main(void)
 {
