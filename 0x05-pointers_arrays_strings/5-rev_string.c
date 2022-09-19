c50ea376059139697b77f8143f495502cbf06344
MusaSkuru <captainskuru@gmail.com>
2022-09-19 15:30:58 -0700

    r string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,19 +5,19 @@
  * @s: input string
  * return: string in reverse
  */
-void rev_string(char *S)
+void rev_string(char *s)
 {
-	char rev = S[0];
+	char rev = s[0];
 	int counter = 0;
 	int i;
 
-	while (S[counter] != '\0')
+	while (s[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
 		rev + s[i];
-		S[i] = s[counter];
-		S[counter] = rev;
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
