cf8f2af4f9497f1872ccc7d6f0cf3dac50c5ff92
MusaSkuru <captainskuru@gmail.com>
2022-09-28 04:00:00 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,32 @@
-/**
-File: 5-strstr.c
-* Auth: Someone who is kind
-*/
 #include "main.h"
+
 /**
-* _strstr - Locates a substring.
-* @haystack: The string to be searched.
-* @needle: The substring to be located.
-**
-Return: If the substring is located - a pointer to the beginning
-* of the located substring.
-* If the substring is not located - NULL.
-*/
+ * _strstr - locates a substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
+ *
+ * Return: pointer to beginning of needle in haystack or NULL if no match
+ */
 char *_strstr(char *haystack, char *needle)
 {
-int index;
-if (*needle == 0)
-return (haystack);
-while (*haystack)
-{
-index = 0;
-if (haystack[index] == needle[index])
-{
-do {
-if (needle[index + 1] == '\0')
-return (haystack);
-index++;
-} while (haystack[index] == needle[index]);
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
 }
-haystack++;
-}
-return ('\0');
