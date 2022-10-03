70da9f78ec50e532b5bd4596d6e7815e8091a2c3
MusaSkuru <captainskuru@gmail.com>
2022-10-03 15:33:39 -0700

    task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#Include <stdlib.h>
+
+int *_putchar(char);
+char *create_array(ungigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int height);
+char **strtow(char *str);
+
+#endif /* MAIN_H*\
