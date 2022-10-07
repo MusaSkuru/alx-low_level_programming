bee5ef2b4a34972842dd526732bc1a9e21428fcb
MusaSkuru <captainskuru@gmail.com>
2022-10-07 15:24:57 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,17 +8,16 @@
 */
 char *create_array(unsigned int size, char c)
 {
-	char *str;
 	unsigned int i;
+	char *s;
 
-	str = malloc(sizeof(char) * size);
-
-	if (size == 0 || str == NULL)
+	if (size == 0)
 		return (NULL);
-
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
-		str[i] = c;
+		s[i] = c;
 
-	return (str);
+	return (s);
 }
-
